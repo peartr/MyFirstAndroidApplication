cd868dd556fb67ad3a41dc6acc88fa5d2a72c125
Ramon Peart <ramon.peart@gmail.com>
2018-09-01 15:07:01 -0400

    Initial commit of my first Android application.

---
+++ app/src/main/java/com/example/ramon/myfirstandroidapplication/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.ramon.myfirstandroidapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_android_studio:
+                Toast.makeText(this, "Android Studio Rules!", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_my_first_app:
+                Toast.makeText(this, "My First App is the best ever!", Toast.LENGTH_LONG).show();
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
+    }
+}
